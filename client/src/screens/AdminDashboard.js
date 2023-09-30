c682d1b4ca394b89ccc3002327b9b1a54eeb39ac
JOVILCHESC <rockyone109@gmail.com>
2023-09-29 23:11:15 -0300

    login y registro por roles se supone que completo

--- client/src/screens/AdminDashboard.js
+++ client/src/screens/AdminDashboard.js
@@ -1,8 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../helpers/newAuthContext';
+import axios from 'axios'; // Importa Axios
 
 const AdminDashboard = () => {
-  const { authState } = useContext(AuthContext);
+  const { authState, setAuthState } = useContext(AuthContext); // Importa setAuthState del contexto
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Realiza la carga de datos del usuario de forma síncrona
+    axios
+      .get('http://localhost:3000/auth/auth', {
+        headers: {
+          accessToken: localStorage.getItem('accessToken'),
+        },
+      })
+      .then((response) => {
+        if (!response.data.error) {
+          setAuthState({
+            username: response.data.username,
+            id: response.data.id,
+            status: true,
+            role: response.data.role,
+          });
+        }
+        setIsLoading(false); // Marca la carga como completa
+      });
+  }, [setAuthState]); // Agrega setAuthState al arreglo de dependencias
+  
+
+  // Muestra un indicador de carga mientras se obtienen los datos del usuario
+  if (isLoading) {
+    return (
+      <div>
+        <h1 className="text-center">Loading...</h1>
+      </div>
+    );
+  }
 
   // Verifica si el usuario tiene permisos de administrador antes de renderizar el contenido
   if (authState.role !== 'admin') {
@@ -13,12 +46,12 @@ const AdminDashboard = () => {
     );
   }
 
-  else {return (
+  return (
     <div>
       <h1 className="text-center">Admin Dashboard</h1><br/>
       {/* Agrega aquí el contenido del dashboard */}
     </div>
   );
-}}
+}
 
 export default AdminDashboard;
